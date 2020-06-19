7dfbee131bc54f29052e701f71c63cba5b5d478c
ronaldoresende <64918667+ronaldoresende@users.noreply.github.com>
2020-06-19 12:30:01 -0300

    Playbook apache
    Instalaçao de apache utilizando ansible com modelo de arquivo index enviando para o servidor de apache

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>Instalação de Apache via Ansible</title>
+  </head>
+  <body>
+  <h1>Apache instalado via Ansible</h1>
+
+
+
+
+<?php
+$ip = gethostbyname('apache');
+
+echo "IP: " .$ip;
+?>
+
+
+  </body>
+</html>
